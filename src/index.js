474be62f7d5a2d8752514ca1ea7a2ac70d589736
Omar Amin <omar_75@hotmail.dk>
2020-02-17 21:26:58 +0100

    Setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import LogIn from './pages/LogIn';
+
+ReactDOM.render(<LogIn />, document.getElementById('root'));
---
+++ src/pages/LogIn.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+class LogIn extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+export default LogIn
