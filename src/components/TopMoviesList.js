755c53d2c51a16cc06b7524684bcc6b59a3c7930
Omar Amin <omar_75@hotmail.dk>
2020-02-27 17:59:17 +0100

    Layout design for "Home"

---
+++ src/components/TopMoviesList.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+class TopMoviesList extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                list
+            </div>
+        )
+    }
+}
+
+export default TopMoviesList
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from "react"
 import base from "../auth/base"
+import "./Home.css"
+import TopMoviesList from "../components/TopMoviesList"
 
 class Home extends React.Component {
     constructor() {
@@ -9,11 +11,16 @@ class Home extends React.Component {
         }
     }
 
+    //Search should add movies?
     render() {
         return (
-            <div>
-                Home
-                <button onClick={() => base.auth().signOut()}>Sign out</button>
+            <div className="container">
+                <h1 className="title">Your top movies</h1>
+                <div className="search-bar">[implement search]</div>
+                <div className="top-movies-list">
+                    <TopMoviesList></TopMoviesList>
+                </div>
+                <button className="sign-out-button" onClick={() => base.auth().signOut()}>Sign out</button>
             </div>
         )
     }
